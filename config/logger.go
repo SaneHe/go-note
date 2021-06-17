e8d96e415af7cf2d829e0e6b4c867a43c3cea279
sane <18217108467@163.com>
2021-06-17 10:20:30 +0800

    init

--- config/logger.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Logger struct {
-	Debug              string `yaml:"debug"`
-	ConsoleColor       bool   `yaml:"consoleColor"`
-	LogFileName        string `yaml:"logFileName"`
-	ReportCaller       bool   `yaml:"reportCaller"`
-	LogTimestampFormat string `yaml:"logTimestampFormat"`
-}
--- config/redis.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type RedisConfig struct {
-	Host     string `yaml:"host"`
-	Password string `yaml:"passwd"`
-	DB       int    `yaml:"database"`
-}
